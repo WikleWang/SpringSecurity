772991347aa448b9cd30a19e3e009a839c3f6283
wikle.wang <wangwei@amt.com.cn>
2019-12-22 19:32:32 +0800

    SpringBoot集成Spring Security: 4. 自定义表单登录

--- src/main/java/com/amt/mysecurity/MySecurityApplication.java
+++ src/main/java/com/amt/mysecurity/MySecurityApplication.java
@@ -1,13 +1,26 @@
 package com.amt.mysecurity;
 
+import com.amt.mysecurity.web.VerifyServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MySecurityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MySecurityApplication.class, args);
+    }
+
+    /**
+     * 注入验证码servlet
+     */
+    @Bean
+    public ServletRegistrationBean indexServletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new VerifyServlet());
+        registration.addUrlMappings("/getVerifyCode");
+        return registration;
     }
 
 }
--- src/main/java/com/amt/mysecurity/security/WebSecurityConfig.java
+++ src/main/java/com/amt/mysecurity/security/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.amt.mysecurity.security;
 
+import com.amt.mysecurity.web.VerifyFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,9 +12,12 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
 
 @Configuration
@@ -35,44 +40,87 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
+    @Autowired
+    private CustomAuthenticationProvider customAuthenticationProvider;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(new PasswordEncoder() {
-            @Override
-            public String encode(CharSequence charSequence) {
-                return charSequence.toString();
-            }
-
-            @Override
-            public boolean matches(CharSequence charSequence, String s) {
-                return s.equals(charSequence.toString());
-            }
-        });
+        auth.authenticationProvider(customAuthenticationProvider);
     }
+
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailsService).passwordEncoder(new PasswordEncoder() {
+//            @Override
+//            public String encode(CharSequence charSequence) {
+//                return charSequence.toString();
+//            }
+//
+//            @Override
+//            public boolean matches(CharSequence charSequence, String s) {
+//                return s.equals(charSequence.toString());
+//            }
+//        });
+//    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                // 如果有允许匿名的url，填在下面
+//                .antMatchers("/getVerifyCode").permitAll()
+//                // 设置比如通过验证的url
+//                .anyRequest().authenticated()
+//                .and()
+//                // 设置登陆页
+//                .formLogin().loginPage("/login")
+//                // 设置登陆成功页
+//                .defaultSuccessUrl("/").permitAll()
+//                // 设置登录失败url
+//                .failureUrl("/login/error")
+//                // 自定义登陆用户名和密码参数，默认为username和password
+//                //.usernameParameter("username")
+//                //.passwordParameter("password")
+//                .and()
+//                .addFilterBefore(new VerifyFilter(), UsernamePasswordAuthenticationFilter.class)
+//                .logout().permitAll()
+//                // 自动登录 Cookie 存储
+//                .and().rememberMe()
+//                // 用数据库保存
+//                .tokenRepository(persistentTokenRepository())
+//                // 有效时间：单位s
+//                .tokenValiditySeconds(60)
+//                .userDetailsService(userDetailsService);
+//
+//        // 关闭CSRF跨域
+//        http.csrf().disable();
+//    }
+
+    @Autowired
+    private AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> authenticationDetailsSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 // 如果有允许匿名的url，填在下面
-                //.antMatchers().permitAll()
-                // 设置比如通过验证的url
+                .antMatchers("/getVerifyCode").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 // 设置登陆页
                 .formLogin().loginPage("/login")
                 // 设置登陆成功页
                 .defaultSuccessUrl("/").permitAll()
-                // 设置登录失败url
+                // 登录失败Url
                 .failureUrl("/login/error")
                 // 自定义登陆用户名和密码参数，默认为username和password
-                //.usernameParameter("username")
-                //.passwordParameter("password")
+//                .usernameParameter("username")
+//                .passwordParameter("password")
+                // 指定authenticationDetailsSource
+                .authenticationDetailsSource(authenticationDetailsSource)
                 .and()
                 .logout().permitAll()
-                // 自动登录 Cookie 存储
+                // 自动登录
                 .and().rememberMe()
-                // 用数据库保存
                 .tokenRepository(persistentTokenRepository())
                 // 有效时间：单位s
                 .tokenValiditySeconds(60)
@@ -81,6 +129,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // 关闭CSRF跨域
         http.csrf().disable();
     }
+
+
+
+
 
     @Override
     public void configure(WebSecurity web) throws Exception {
